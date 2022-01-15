9cb18cd72a672fed6e7602511cec0efd7741507a
Abdulaziz Al-sharji <66184929+abdulaziz5alsharji@users.noreply.github.com>
2022-01-15 16:44:07 +0400

    Create lesson_2.js

---
+++ lesson_2.js
@@ -0,0 +1,42 @@
+// Lesson 2
+console.log(typeof "Hello World")
+console.log(typeof 44)
+console.log(typeof 22.2)
+console.log(typeof ["Hello",  "World", 1])
+console.log(typeof {name: "Ali", age: 12})
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof false)
+
+var user = "Ali", age = 12;
+window.alert(`Your name is: ${user}, and your age is ${age}`);
+document.write(`<h1>Your name is: ${user}, and your age is ${age}</h1>`)
+console.log(`Your name is: ${user}, and your age is ${age}`)
+console.log(Js)
+
+let name = "Abdulaziz";
+console.log("My \\Name Is : \n"+ name + "\t\"Al-sharji\"");
+
+console.log("Hello\
+ World\
+ ,\ HI Js")
+
+let msg1 = "I lOve";
+let msg2 = "JavaScript";
+let space = " ";
+console.log(msg1 + space + msg2);
+console.log(numberOne + numberTwo);
+
+var numberOne = 10;
+var numberTwo = 20;
+
+console.log(numberOne + "" + numberTwo);
+console.log(typeof (numberOne + "" + numberTwo));
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof (`${numberOne}${numberTwo}`));
+console.log(numberOne + "\n" + numberTwo);
+console.log(`${numberOne}\n${numberTwo}`);
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
