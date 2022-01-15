7b28daedcfc6af8f0ca0cc04ff39d5e6f028071f
Abdulaziz Al-sharji <66184929+abdulaziz5alsharji@users.noreply.github.com>
2022-01-15 16:42:17 +0400

    Create lesson_1.js

---
+++ lesson_1.js
@@ -0,0 +1,29 @@
+// Lesson 1
+window.alert("Hello World");
+document.write("<h1>Hello World </h1>");
+console.log("Hello World");
+// console 
+//     - log
+//     - error
+//     - table
+console.error("Hello World")
+document.write("<h1 style='color:blue;font-size:80px;font-weight:bold;text-align: center;font-family: Arial'>Elzero</h1>");
+console.log("%cElzero %cWeb %cSchool", "color:red;font-size:40px", "color:green;font-weight:bold;font-size:40px", "color:blue;font-size:40px;")
+window.alert("Hello World");
+window.confirm("Do Want is clone ??");
+window.close();
+console.log("%cHello %cWorld", "color:red;", "color:blue;");
+console.log("%cHello %cWorld", "color:red;", "color:blue;");
+console.group("First");
+console.log("%cHello %cWorld", "color:red;", "color:blue;");
+console.log("%cHello %cWorld", "color:red;", "color:blue;");
+console.group("Second");
+console.log("%cHello %cWorld", "color:red;", "color:blue;");
+console.log("%cHello %cWorld", "color:red;", "color:blue;");
+console.group("Third");
+console.table(["Ali", "Abdulaziz", "Faisal"])
+console.error("Error");
+console.assert(4 == 3, "ERROR");
+var name = "Ali";
+console.log("Your name is: " + name);
+console.log(`Your name is ${name}`);
